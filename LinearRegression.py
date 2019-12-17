aad41ebf495d9efd41faf0562d26c6e26b7dfa70
Keroles <kerolesmonsef@gmail.com>
2019-12-17 16:19:31 +0200

    first

---
+++ LinearRegression.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import math
+
+df = pd.read_csv("Data/wiki.csv")
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100
+
