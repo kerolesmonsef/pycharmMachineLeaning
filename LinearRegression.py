4bff34fe161a76f9189630a10e6f630dd918fe4e
Keroles <kerolesmonsef@gmail.com>
2019-12-17 16:34:30 +0200

    real data

--- LinearRegression.py
+++ LinearRegression.py
@@ -5,4 +5,6 @@ import math
 df = pd.read_csv("Data/wiki.csv")
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
 
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
