408e6fef2c2bf3626a01453341f34c5f115e1d46
Keroles <kerolesmonsef@gmail.com>
2019-12-17 17:40:39 +0200

    update Dataset from 2000 to 2019

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import math
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv("Data/wiki.csv")
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
@@ -8,3 +11,21 @@ df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+forcast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+
+forcast_out = int(math.ceil(0.01 * len(df)))
+df['label'] = df[forcast_col].shift(-forcast_out)
+df.dropna(inplace=True)
+
+X = np.array(df.drop('label', 1))
+y = np.array(df['label'])
+
+X = preprocessing.scale(X)
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(x_train, y_train)
+accuracy = clf.score(x_test, y_test)
+print(accuracy)
