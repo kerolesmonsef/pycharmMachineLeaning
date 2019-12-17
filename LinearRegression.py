d9b37dbdd02616e219d124ce526251d9091cdace
Keroles <kerolesmonsef@gmail.com>
2019-12-17 19:09:43 +0200

    ghg

--- LinearRegression.py
+++ LinearRegression.py
@@ -15,17 +15,22 @@ forcast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
 forcast_out = int(math.ceil(0.01 * len(df)))
-df['label'] = df[forcast_col].shift(-forcast_out)
-df.dropna(inplace=True)
-
+df['label'] = df[forcast_col].shift(-forcast_out)  # the DF contain Nan In the End
 X = np.array(df.drop('label', 1))
-y = np.array(df['label'])
-
 X = preprocessing.scale(X)
+X = X[:-forcast_out]  # The X does Not Contain NaN
+X_lately = X[-forcast_out:]
 
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+df.dropna(inplace=True)
+y = np.array(df['label'])
+# X = preprocessing.scale(X)
 
-clf = LinearRegression()
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+
+clf = LinearRegression(n_jobs=-1)
 clf.fit(x_train, y_train)
 accuracy = clf.score(x_test, y_test)
 print(accuracy)
+forcast_set = clf.predict(X_lately)
+print(forcast_set)
