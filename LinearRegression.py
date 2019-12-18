27865ac95c660d2a660bf6bc68e5596c3bf7269b
Keroles <kerolesmonsef@gmail.com>
2019-12-18 16:35:35 +0200

    finish regrestions

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,36 +1,63 @@
 import pandas as pd
 import numpy as np
-import math
+import math, datetime
 from sklearn import preprocessing, svm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+from sklearn.metrics import accuracy_score;
 
-df = pd.read_csv("Data/wiki.csv")
-df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+
+style.use('ggplot')
+
+df = pd.read_csv("Data/wik2000-2019i.csv")
+df.index = df['Date']
+df = df[['Date', 'Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
 
-df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
-forcast_col = 'Adj. Close'
+df = df[['Date', 'Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
-forcast_out = int(math.ceil(0.01 * len(df)))
-df['label'] = df[forcast_col].shift(-forcast_out)  # the DF contain Nan In the End
-X = np.array(df.drop('label', 1))
+forecast_out = int(math.ceil(0.01 * len(df)))
+df['label'] = df[forecast_col].shift(-forecast_out)  # the DF contain Nan In the End
+X = np.array(df.drop(['label', 'Date'], 1))
 X = preprocessing.scale(X)
-X = X[:-forcast_out]  # The X does Not Contain NaN
-X_lately = X[-forcast_out:]
+X_lately = X[-forecast_out:]  # get the Xs that contains (NaN Ys)
+X = X[:-forecast_out]  # The X does Not Contain NaN ys
 
 df.dropna(inplace=True)
 y = np.array(df['label'])
 # X = preprocessing.scale(X)
 
 
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=30)
 
 clf = LinearRegression(n_jobs=-1)
 clf.fit(x_train, y_train)
 accuracy = clf.score(x_test, y_test)
-print(accuracy)
-forcast_set = clf.predict(X_lately)
-print(forcast_set)
+print("Accuracy = ",accuracy)
+forecast_set = clf.predict(X_lately)
+y_predicted = clf.predict(x_train)
+print("real prediction", y_predicted)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1]['Date']
+last_unix = datetime.datetime.fromisoformat(last_date).timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_day = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_day] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df[forecast_col].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel("Date")
+plt.ylabel('Price')
+plt.show()
